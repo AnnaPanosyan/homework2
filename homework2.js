7126bdc3832ec5df87b01b2b221b28ea7f6c0b1b
AnnaPanosyan <80696532+AnnaPanosyan@users.noreply.github.com>
2021-05-24 12:56:16 -0700

    Add files via upload

---
+++ homework2.js
@@ -0,0 +1,16 @@
+//task1
+// Given a number. Print “odd” if the number is odd and “even” if it’s even.
+
+let number =+prompt ("Enter a number");
+    if ( number % 2 === 1 ) { 
+         console.log( "odd" )
+     }else{
+         console.log( "even" ) 
+     }
+
+//task2
+// Given a positive integer. Bring the last digit of the number to the beginning. Print the new
+// number. If the last digit of the inserted number is 0, number remains the same.
+
+let number =+prompt ("Enter a number");
+
