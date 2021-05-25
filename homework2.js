594b4e1a20648e51b7cf7593cde1d226ca321c04
AnnaPanosyan <80696532+AnnaPanosyan@users.noreply.github.com>
2021-05-25 08:16:05 -0700

    Update homework2.js

--- homework2.js
+++ homework2.js
@@ -12,17 +12,38 @@ let number =+prompt ("Enter a number");
 // Given a positive integer. Bring the last digit of the number to the beginning. Print the new
 // number. If the last digit of the inserted number is 0, number remains the same.
 
-let number =+prompt ("Enter a number");
 let number = +prompt ("Enter a number");
 let newNumber;
-    if (number > 0 &&  number === Math.round(number) ){
-        if (number === Math.round(number) || number % 10 === 0 ) {
+    if (number > 0 && number === Math.floor(number) ){
+        if (number % 10 === 0 || number % 10 === number ) {
             console.log(number);
-            }else{
-                newNumber = " " + number % 10 + (number - number % 10) / 10;
-                console.log(newNumber)
-                }
-        } else{
-            console.log("Wrong number")
-        }
+        }else{
+            newNumber = " " + number % 10 + (number - number % 10) / 10;
+            console.log(newNumber);
+            }
+    } else{
+        console.log("Wrong number");
+    }
+// task3
+// Given five numbers as input. Calculate and print the average of the numbers(without
+// using arrays).
+
+let number1 = +prompt ("Enter a number");
+let number2 = +prompt ("Enter a number");
+let number3 = +prompt ("Enter a number");
+let number4 = +prompt ("Enter a number");
+let number5 = +prompt ("Enter a number"); 
+let average = (number1 + number2 + number3 + number4 + number5)/5;
+    console.log (average);
+
+// task4
+// Given three numbers. Sort them by the ascending order.
+let number1 = +prompt ("Enter a number");
+let number2 = +prompt ("Enter a number");
+let number3 = +prompt ("Enter a number");
+ if (numbe1 >= number2 $$ number1 >= number3 && number2 >= number3) {
+    console.log(number1,number2,number3)
+}
+
+
 
